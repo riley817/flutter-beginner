839d258ecbeaa36e318989f20d7147c0963372fe
riley817 <riley0422@icloud.com>
2023-02-12 22:44:21 +0900

    20230212

---
+++ random_number_generator/lib/component/number_row.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NumberRow extends StatelessWidget {
+  final int number;
+  const NumberRow({required this.number, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+        children: number
+          .toString()
+          .split('')
+          .map((y) => Image.asset('asset/img/$y.png', height: 70.0, width: 50.0))
+          .toList());
+  }
+}
